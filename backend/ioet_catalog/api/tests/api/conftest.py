2f342699429a3283e8d87c2e81e84b8288bc4f06
JosueOb <josuericardocando@hotmail.com>
2024-03-16 12:36:51 -0500

    Create fixture to initialize db session

--- backend/ioet_catalog/api/tests/api/conftest.py
+++ backend/ioet_catalog/api/tests/api/conftest.py
@@ -1,6 +1,9 @@
+from typing import Generator
+
 import pytest
 from fastapi.testclient import TestClient
 
+from adapters.src import Connection, SessionManager, SQLConnection
 from api.src.create_app import create_app
 
 
@@ -10,3 +13,15 @@ def api_client() -> TestClient:
     client = TestClient(api)
 
     return client
+
+
+@pytest.fixture(autouse=True)
+def initialize_session() -> Generator[None, None, None]:
+    # The FastAPI's TestClient does not trigger the lifespan events,
+    # which means the session initialization and closing in the lifespan
+    # function are not being executed during testing. So for that reason,
+    # this fixture has been created.
+    connection: Connection = SQLConnection()
+    SessionManager.initialize_session(connection)
+    yield
+    SessionManager.close_session()
