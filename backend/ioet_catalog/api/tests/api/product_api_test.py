93408f3c23455a48f487269980d120532568eb5a
Diego Rojas <drojas@seatgeek.com>
2024-03-28 00:13:24 -0500

    solve all issues

--- backend/ioet_catalog/api/tests/api/product_api_test.py
+++ backend/ioet_catalog/api/tests/api/product_api_test.py
@@ -2,11 +2,9 @@ from http import HTTPStatus
 
 from fastapi.testclient import TestClient
 
-
 def test__should_return_a_list_of_products(api_client: TestClient):
     # Take a look at this test, what do you think could be improved?
     response = api_client.get("/products/")
     products = response.json().get("products")
-
     assert response.status_code == HTTPStatus.OK
-    assert products
+    assert products == []
